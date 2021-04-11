d0ccbbe8fbbbde4b79f21e1108901734286a51fc
HelirH <hoop.helir@gmail.com>
2021-04-11 13:39:34 +0300

    0.00.1

---
+++ 2D Platformer/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public SpriteRenderer sp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        Debug.Log(xInput);
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        if (xInput < 0)
+        {
+            sp.flipX = true;
+        }
+        else
+        {
+            sp.flipX = false;
+        }
+ 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
