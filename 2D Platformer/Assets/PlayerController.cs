28f4d60b4ab6bc088fcbeb6aa454c4db1bb29a53
HelirH <hoop.helir@gmail.com>
2021-04-18 13:47:55 +0300

    0.00.1

--- 2D Platformer/Assets/PlayerController.cs
+++ 2D Platformer/Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +7,19 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public float jumpHeight;
     public Rigidbody2D rb;
     public SpriteRenderer sp;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
         float xInput = Input.GetAxis("Horizontal");
-        Debug.Log(xInput);
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
         if (xInput < 0)
         {
@@ -28,11 +29,20 @@ public class PlayerController : MonoBehaviour
         {
             sp.flipX = false;
         }
- 
+
     }
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Force);
+        }
+    }
 
+    bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+        return hit.collider != null;
     }
 }
